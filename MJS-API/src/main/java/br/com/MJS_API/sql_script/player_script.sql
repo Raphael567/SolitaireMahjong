3ee4811c56f9d07fd2d2bfb41307e275dbd00a32
Raphael567 <raphaelcanuto464@gmail.com>
2024-09-19 12:28:55 -0300

    feat:Insert na tabela Peca

--- MJS-API/src/main/java/br/com/MJS_API/sql_script/player_script.sql
+++ MJS-API/src/main/java/br/com/MJS_API/sql_script/player_script.sql
@@ -20,3 +20,38 @@ INSERT INTO Player (nome) VALUES ('Ana Souza');
 INSERT INTO Player (nome, pontuacao) VALUES ('Mariana Oliveira', 2000);
 INSERT INTO Player (nome) VALUES ('Pedro Mendes');
 
+
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Vermelho', 'Leque', 1);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Vermelho', 'Lâmpada', 1);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Vermelho', 'Dragão', 1);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Vermelho', 'Caractere', 1);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Vermelho', 'Flor', 1);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Vermelho', 'Carpa', 1);
+
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Amarelo', 'Leque', 2);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Amarelo', 'Lâmpada', 2);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Amarelo', 'Dragão', 2);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Amarelo', 'Caractere', 2);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Amarelo', 'Flor', 2);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Amarelo', 'Carpa', 2);
+
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Azul', 'Leque', 3);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Azul', 'Lâmpada', 3);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Azul', 'Dragão', 3);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Azul', 'Caractere', 3);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Azul', 'Flor', 3);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Azul', 'Carpa', 3);
+
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Verde', 'Leque', 5);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Verde', 'Lâmpada', 5);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Verde', 'Dragão', 5);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Verde', 'Caractere', 5);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Verde', 'Flor', 5);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Verde', 'Carpa', 5);
+
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Roxo', 'Leque', 10);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Roxo', 'Lâmpada', 10);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Roxo', 'Dragão', 10);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Roxo', 'Caractere', 10);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Roxo', 'Flor', 10);
+INSERT INTO Peca (cor, simbolo, pontuacao) VALUES ('Roxo', 'Carpa', 10);
