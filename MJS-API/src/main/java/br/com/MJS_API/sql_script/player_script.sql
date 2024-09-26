d8cd84f0cc1eeb7c63f8602a79fece75958c0a1f
Raphael567 <raphaelcanuto464@gmail.com>
2024-09-26 19:50:28 -0300

    Peças rosas adicionadas na API, inserts para um total de 144 peças e lógica para disposição das peças no layout desejado

--- MJS-API/src/main/java/br/com/MJS_API/sql_script/player_script.sql
+++ MJS-API/src/main/java/br/com/MJS_API/sql_script/player_script.sql
@@ -30,94 +30,185 @@ INSERT INTO Player (nome) VALUES ('Pedro Mendes');
 -- Vermelho
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Leque', 1, 'leque_vermelho.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Leque', 1, 'leque_vermelho.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Leque', 1, 'leque_vermelho.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Leque', 1, 'leque_vermelho.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Luminária', 1, 'luminaria_vermelho.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Luminária', 1, 'luminaria_vermelho.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Luminária', 1, 'luminaria_vermelho.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Luminária', 1, 'luminaria_vermelho.png');
 
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Dragão', 1, 'dragao_vermelho.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Dragão', 1, 'dragao_vermelho.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Dragão', 1, 'dragao_vermelho.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Dragão', 1, 'dragao_vermelho.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Caractere', 1, 'caractere_vermelho.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Caractere', 1, 'caractere_vermelho.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Caractere', 1, 'caractere_vermelho.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Caractere', 1, 'caractere_vermelho.png');
 
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Flor', 1, 'flor_vermelho.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Flor', 1, 'flor_vermelho.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Flor', 1, 'flor_vermelho.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Flor', 1, 'flor_vermelho.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Carpa', 1, 'carpa_vermelho.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Carpa', 1, 'carpa_vermelho.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Carpa', 1, 'carpa_vermelho.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Vermelho', 'Carpa', 1, 'carpa_vermelho.png');
 
 -- Amarelo
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Leque', 2, 'leque_amarelo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Leque', 2, 'leque_amarelo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Leque', 2, 'leque_amarelo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Leque', 2, 'leque_amarelo.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Luminária', 2, 'luminaria_amarelo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Luminária', 2, 'luminaria_amarelo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Luminária', 2, 'luminaria_amarelo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Luminária', 2, 'luminaria_amarelo.png');
 
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Dragão', 2, 'dragao_amarelo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Dragão', 2, 'dragao_amarelo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Dragão', 2, 'dragao_amarelo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Dragão', 2, 'dragao_amarelo.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Caractere', 2, 'caractere_amarelo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Caractere', 2, 'caractere_amarelo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Caractere', 2, 'caractere_amarelo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Caractere', 2, 'caractere_amarelo.png');
 
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Flor', 2, 'flor_amarelo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Flor', 2, 'flor_amarelo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Flor', 2, 'flor_amarelo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Flor', 2, 'flor_amarelo.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Carpa', 2, 'carpa_amarelo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Carpa', 2, 'carpa_amarelo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Carpa', 2, 'carpa_amarelo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Amarelo', 'Carpa', 2, 'carpa_amarelo.png');
 
 -- Azul
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Leque', 3, 'leque_azul.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Leque', 3, 'leque_azul.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Leque', 3, 'leque_azul.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Leque', 3, 'leque_azul.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Luminária', 3, 'luminaria_azul.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Luminária', 3, 'luminaria_azul.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Luminária', 3, 'luminaria_azul.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Luminária', 3, 'luminaria_azul.png');
 
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Dragão', 3, 'dragao_azul.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Dragão', 3, 'dragao_azul.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Dragão', 3, 'dragao_azul.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Dragão', 3, 'dragao_azul.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Caractere', 3, 'caractere_azul.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Caractere', 3, 'caractere_azul.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Caractere', 3, 'caractere_azul.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Caractere', 3, 'caractere_azul.png');
 
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Flor', 3, 'flor_azul.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Flor', 3, 'flor_azul.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Flor', 3, 'flor_azul.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Flor', 3, 'flor_azul.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Carpa', 3, 'carpa_azul.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Carpa', 3, 'carpa_azul.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Carpa', 3, 'carpa_azul.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Azul', 'Carpa', 3, 'carpa_azul.png');
 
 -- Verde
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Leque', 5, 'leque_verde.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Leque', 5, 'leque_verde.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Leque', 5, 'leque_verde.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Leque', 5, 'leque_verde.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Luminária', 5, 'luminaria_verde.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Luminária', 5, 'luminaria_verde.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Luminária', 5, 'luminaria_verde.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Luminária', 5, 'luminaria_verde.png');
 
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Dragão', 5, 'dragao_verde.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Dragão', 5, 'dragao_verde.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Dragão', 5, 'dragao_verde.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Dragão', 5, 'dragao_verde.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Caractere', 5, 'caractere_verde.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Caractere', 5, 'caractere_verde.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Caractere', 5, 'caractere_verde.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Caractere', 5, 'caractere_verde.png');
 
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Flor', 5, 'flor_verde.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Flor', 5, 'flor_verde.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Flor', 5, 'flor_verde.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Flor', 5, 'flor_verde.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Carpa', 5, 'carpa_verde.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Carpa', 5, 'carpa_verde.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Carpa', 5, 'carpa_verde.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Verde', 'Carpa', 5, 'carpa_verde.png');
 
 -- Roxo
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Leque', 10, 'leque_roxo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Leque', 10, 'leque_roxo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Leque', 10, 'leque_roxo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Leque', 10, 'leque_roxo.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Luminária', 10, 'luminaria_roxo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Luminária', 10, 'luminaria_roxo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Luminária', 10, 'luminaria_roxo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Luminária', 10, 'luminaria_roxo.png');
 
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Dragão', 10, 'dragao_roxo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Dragão', 10, 'dragao_roxo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Dragão', 10, 'dragao_roxo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Dragão', 10, 'dragao_roxo.png');
 
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Caractere', 10, 'caractere_roxo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Caractere', 10, 'caractere_roxo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Caractere', 10, 'caractere_roxo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Caractere', 10, 'caractere_roxo.png');
 
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Flor', 10, 'flor_roxo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Flor', 10, 'flor_roxo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Flor', 10, 'flor_roxo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Flor', 10, 'flor_roxo.png');
 
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Carpa', 10, 'carpa_roxo.png');
 INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Carpa', 10, 'carpa_roxo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Carpa', 10, 'carpa_roxo.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Roxo', 'Carpa', 10, 'carpa_roxo.png');
+
+-- Rosa
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Leque', 10, 'leque_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Leque', 10, 'leque_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Leque', 10, 'leque_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Leque', 10, 'leque_rosa.png');
+
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Luminária', 10, 'luminaria_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Luminária', 10, 'luminaria_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Luminária', 10, 'luminaria_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Luminária', 10, 'luminaria_rosa.png');
+
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Dragão', 10, 'dragao_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Dragão', 10, 'dragao_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Dragão', 10, 'dragao_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Dragão', 10, 'dragao_rosa.png');
+
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Caractere', 10, 'caractere_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Caractere', 10, 'caractere_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Caractere', 10, 'caractere_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Caractere', 10, 'caractere_rosa.png');
+
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Flor', 10, 'flor_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Flor', 10, 'flor_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Flor', 10, 'flor_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Flor', 10, 'flor_rosa.png');
+
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Carpa', 10, 'carpa_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Carpa', 10, 'carpa_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Carpa', 10, 'carpa_rosa.png');
+INSERT INTO Peca (cor, simbolo, pontuacao, imagem) VALUES ('Rosa', 'Carpa', 10, 'carpa_rosa.png');
--- SolitaireMahjongApp/ViewModels/MahjongViewModel.cs
+++ SolitaireMahjongApp/ViewModels/MahjongViewModel.cs
@@ -19,21 +19,18 @@ namespace SolitaireMahjongApp.ViewModels
         private readonly TileService _tileService;
 
         [ObservableProperty]
-        private string timerText = "Time 2:00";
+        private string timerText;
 
         [ObservableProperty]
-        private string scoreText = "Score: 0";
+        private string scoreText;
 
         [ObservableProperty]
         private ObservableCollection<Tile> _tiles;
 
-        //[ObservableProperty]
-        //private ObservableCollection<Tile> tiles = new ObservableCollection<Tile>();
-
         private Tile _firstTileSelected = null;
         private Tile _secondTileSelected = null;
         private int _score = 0;
-        private int _timeLeft = 120; // 2 minutos em segundos
+        private int _timeLeft = 20000; // 2 minutos em segundos
 
         public ICommand TileCommand { get; }
 
@@ -42,7 +39,12 @@ namespace SolitaireMahjongApp.ViewModels
             _tileService = new TileService();
             TileCommand = new RelayCommand<Tile>(OnTileClicked);
             LoadTilesCommand = new AsyncRelayCommand(LoadTilesAsync);
-            //StartTimer();
+
+            Task.Run(async () =>
+            {
+                await LoadTilesAsync();
+                StartTimer();
+            });
         }
 
         public IAsyncRelayCommand LoadTilesCommand { get; }
@@ -54,57 +56,138 @@ namespace SolitaireMahjongApp.ViewModels
 
             Tiles = new ObservableCollection<Tile>();
 
-            // Definindo o número de peças por camada
-            int[] tilesPerLayer = { 40, 16, 4 }; // Número de peças por camada
-            int layers = tilesPerLayer.Length; // Total de camadas
-            double tileWidth = 100; // Largura da peça
-            double tileHeight = 150; // Altura da peça
-            double zSpacing = 10; // Espaçamento entre as camadas
+            int[,] layer1 = new int[,]
+            {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            };
 
-            // Calcular o centro do layout
+            int[,] layer2 = new int[,]
+            {
+                { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+                { 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+                { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+                { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 },
+                { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+                { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0 }
+            };
+
+            int[,] layer3 = new int[,]
+            {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            };
+
+            int[,] layer4 = new int[,]
+            {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            };
+
+            int[,] layer5 = new int[,]
+            {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            };
+
+            int[,] layer6 = new int[,]
+            {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            };
+
+            // Define a organização das camadas
+            var layers = new List<int[,]>
+            {
+                layer1, // Camada 1
+                layer2, // Camada 2
+                layer3, // Camada 3
+                layer4, // Camada 4
+                layer5, // Camada 5
+                layer6  // Camada 6
+            };
+
+            int tileWidth = 60;
+            int tileHeight = 75;
+            int zSpacing = 5; // Espaçamento entre as camadas
+
+            //Ajusta ao centro da tela
             double centerX = 300;
             double centerY = 400;
 
-            for (int layer = 0; layer < layers; layer++)
+            int tileIndex = 0;
+            for (int z = 0; z < layers.Count; z++)
             {
-                for (int index = 0; index < tilesPerLayer[layer]; index++)
+                var layer = layers[z];
+                for (int y = 0; y < layer.GetLength(0); y++)
                 {
-                    int tileIndex = layer == 0
-                        ? index // Para a camada 1, usa o índice diretamente
-                        : index + (tilesPerLayer.Take(layer).Sum()); // Para camadas superiores, soma as peças das camadas anteriores
+                    for (int x = 0; x < layer.GetLength(1); x++)
+                    {
+                        if (layer[y, x] == 1 && tileIndex < shuffledTiles.Count)
+                        {
+                            var tile = shuffledTiles[tileIndex];
+                            tileIndex++;
 
-                    // Verifica se há peças suficientes
-                    if (tileIndex >= shuffledTiles.Count)
-                        break;
+                            double posX = centerX - (layer.GetLength(1) * tileWidth / 2) + x * tileWidth;
+                            double posY = centerY - (layer.GetLength(0) * tileHeight / 2) + y * tileHeight - z * zSpacing;
 
-                    var tile = shuffledTiles[tileIndex];
+                            tile.LayoutBounds = new Rect(posX, posY, tileWidth, tileHeight);
 
-                    // Calcula a posição X e Y com base na camada e no índice
-                    double x = centerX - (layer == 0 ? 6 : (layer == 1 ? 4 : 2)) * tileWidth / 2 + (index % (layer == 0 ? 6 : (layer == 1 ? 4 : 2))) * tileWidth;
-                    double y = centerY - (layer == 0 ? 6 : (layer == 1 ? 4 : 2)) * tileHeight / 2 + (index / (layer == 0 ? 6 : (layer == 1 ? 4 : 2))) * tileHeight - (layer * zSpacing);
-
-                    // Define os limites de layout
-                    tile.LayoutBounds = new Rect(x, y, tileWidth, tileHeight);
-
-                    Tiles.Add(tile);
+                            Tiles.Add(tile);
+                        }
+                    }
                 }
             }
         }
 
-        //private async void StartTimer()
-        //{
-        //    while (_timeLeft > 0 && Tiles.Count != 0)
-        //    {
-        //        await Task.Delay(1000);
-        //        _timeLeft--;
-        //        TimerText = $"Time {TimeSpan.FromSeconds(_timeLeft):mm\\:ss}";
+        private async void StartTimer()
+        {
+            while (_timeLeft > 0 && Tiles.Count != 0)
+            {
+                await Task.Delay(1000);
+                _timeLeft--;
+                TimerText = $"Time {TimeSpan.FromSeconds(_timeLeft):mm\\:ss}";
 
-        //        if (_timeLeft == 0)
-        //        {
-        //            GameOver();
-        //        }
-        //    }
-        //}
+                if (_timeLeft == 0)
+                {
+                    GameOver();
+                }
+            }
+        }
 
         private void OnTileClicked(Tile tile)
         {
